4c4465e92410cf340bc01fc8dd1444c1e13d3ae0
EVIDENS Developer <igorcogok@gmail.com>
2025-08-27 19:25:03 -0300

    another one

--- src/components/auth/SplitScreenAuthLayout.tsx
+++ src/components/auth/SplitScreenAuthLayout.tsx
@@ -5,9 +5,9 @@ import React from 'react';
 const SplitScreenAuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-white relative overflow-hidden">
-      <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl px-6">
+      <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl px-8 md:px-6">
         <div className="flex-1 flex justify-center items-center">
-          <div className="w-full max-w-[400px]">
+          <div className="w-2/3 md:w-full max-w-[400px]">
             <div className="flex flex-col">
               <h1 className="font-serif font-medium tracking-tight text-8xl text-black flex items-center">
                 Reviews.
